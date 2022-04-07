a15afa14026a75f5f79881305057e9a4fb9461e8
John McFarland <john.mcfarland222@gmail.com>
2022-04-07 16:57:04 -0700

    made the basic contact page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./styles/main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./layout/Nav";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navigation />
       <Landing />
       <Projects />
+      <Contact />
     </>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import {AiTwotonePhone} from 'react-icons/ai'
+import {MdEmail} from 'react-icons/md'
+const Contact = () => {
+  return (
+    <div className='contact' id='contact'>
+        <div className='contact-form'>
+            <h1>Contact Me</h1>
+            <h2><AiTwotonePhone /> (928)451-6569</h2>
+            <h2><MdEmail /> john.mcfarland222@gmail.com</h2>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Contact
