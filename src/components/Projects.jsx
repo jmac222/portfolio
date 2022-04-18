322d2d6f1f3ae1d49672d9868972dded790a4cf3
John McFarland <Jmcfar187@west-mec.local>
2022-04-18 14:35:35 -0700

    new project layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,126 +1,100 @@
 import React from "react";
 import { Col, Row, Card, Button } from "react-bootstrap";
 import FadeInSection from "./Fade";
-
+import store from '../asset/store.png'
+import allsports from '../asset/allsports.png'
 const Projects = () => {
   return (
     <div className="background">
     <div className="projects" id="projects">
       <h1>My Projects</h1>
       <FadeInSection>
+      <div className="project-container">
 
-      <Row xs={1} md={3} className="g-4 ind">
-        <Col>
-          <Card>
-            <Card.Img
-              className="img"
-              variant="top"
-              src="https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc="
-              width={"50px"}
-            />
-            <Card.Body>
-              <Card.Title>Store Project</Card.Title>
-              <Card.Text>
-                The project implemented use of Node.Js and created a server
-                where you could "buy" RC cars. The project uses MongoDB, Stripe
-                Payment, and Cloudinary. Languages used: JavaScript, CSS, and
-                HTML
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img
-              className="img"
-              variant="top"
-              src="https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc="
-              width={"50px"}
-            />
-            <Card.Body>
-              <Card.Title>TypeScript Calculator</Card.Title>
-              <Card.Text>
-                This project created a calculator that was able to solve
-                geometry problems. Languages used: TypeScript
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img
-              className="img"
-              variant="top"
-              src="https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc="
-              width={"50px"}
-            />
-            <Card.Body>
-              <Card.Title>Breaking Bad API</Card.Title>
-              <Card.Text>
-                You can search characters in the show Breaking Bad. It will give
-                you information on those characters. Languages used: JavaScript,
-                HTML, and CSS
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row xs={1} md={3} className="g-4 ind">
-        <Col>
-          <Card>
-            <Card.Img
-              className="img"
-              variant="top"
-              src="https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc="
-              width={"50px"}
-            />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img
-              className="img"
-              variant="top"
-              src="https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc="
-              width={"50px"}
-            />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img
-              className="img"
-              variant="top"
-              src="https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc="
-              width={"50px"}
-            />
-            <Card.Body id="resume">
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+        <div className="card-row">
+          <div className= 'card'>
+            <div className="back-card card-side">
+              <button onClick={() => {
+                window.open("https://desolate-thicket-49821.herokuapp.com/")
+              }}>View Live Server</button>
+              <button onClick={() => window.open('https://github.com/jmac222/store-project')}>View Source Code</button>
+            </div>
+            <div className="front-card card-side">
+              <img src={store} ></img>
+              <h2>RC Car Vendor</h2>
+              <p>This project allows the user to buy and sell RC cars. The user has the ability to upload a car to sell it. The user can also add to cart and buy it using Stripe. The project uses Node.js! Skills used were: <b>MongoDB, Node.js, Stripe, Javascript, HTML5, CSS3, Libraries</b></p>
+            </div>
+
+          </div>
+          <div className= 'card'>
+            <div className="back-card card-side">
+              <button onClick={() => {
+                window.open("https://sports-all.herokuapp.com/")
+                
+              }}>View Live Server</button>
+              <button onClick={() => window.open('https://github.com/jmac222/sports-page')}>View Source Code</button>
+            </div>
+            <div className="front-card card-side">
+              <img src={allsports}></img>
+              <h2>AllSports API</h2>
+              <p>This project used a sports API to demonstrate the teams records from either the current season or last years season. Leagues included are NFL, NBA, MLB, and NHL. You can also click on the individual teams to see information on that team on the ESPN page. Skills used were: <b>API Usage, CSS, HTML5, React, Javascript</b></p>
+            </div>
+
+          </div>
+          <div className= 'card'>
+            <div className="back-card card-side">
+              <button>View Live Server</button>
+              <button>View Source Code</button>
+            </div>
+            <div className="front-card card-side">
+              <img src="" width='300px' height='200px'></img>
+              <h2>AllSports API</h2>
+              <p>This project used a sports API to demonstrate the teams records from either the current season or last years season. You can also click on individual teams to open up a new tab on information about that team on ESPN. Skills used were: <b>MongoDB, Node.js, Stripe, Javascript, HTML5, CSS3, Libraries</b></p>
+            </div>
+
+          </div>
+        </div>
+        <div className="card-row">
+          <div className= 'card'>
+            <div className="back-card card-side">
+              <button>View Live Server</button>
+              <button>View Source Code</button>
+            </div>
+            <div className="front-card card-side">
+              <img src="" width='300px' height='200px'></img>
+              <h2>Store Project</h2>
+              <p>This project allows the user to buy and sell RC cars. Skills used were: <b>MongoDB, Node.js, Stripe, Javascript, HTML5, CSS3, Libraries</b></p>
+            </div>
+
+          </div>
+          <div className= 'card'>
+            <div className="back-card card-side">
+              <button>View Live Server</button>
+              <button>View Source Code</button>
+            </div>
+            <div className="front-card card-side">
+              <img src="" width='300px' height='200px'></img>
+              <h2>Store Project</h2>
+              <p>This project allows the user to buy and sell RC cars. Skills used were: <b>MongoDB, Node.js, Stripe, Javascript, HTML5, CSS3, Libraries</b></p>
+            </div>
+
+          </div>
+          <div className= 'card'>
+            <div className="back-card card-side">
+              <button>View Live Server</button>
+              <button>View Source Code</button>
+            </div>
+            <div className="front-card card-side">
+              <img src="" width='300px' height='200px'></img>
+              <h2>Store Project</h2>
+              <p>This project allows the user to buy and sell RC cars. Skills used were: <b>MongoDB, Node.js, Stripe, Javascript, HTML5, CSS3, Libraries</b></p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+      
       <div className="button" >
         <Button variant="success" className="btn">
           Resume
